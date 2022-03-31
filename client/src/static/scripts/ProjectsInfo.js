7c5e99cc1f767d28c8babdf7cd2c043f2460a295
Andrew Robles <TheAndrewRobles@yahoo.com>
2022-03-30 23:06:52 -0400

    Updating projects info JSON

--- client/src/static/scripts/ProjectsInfo.js
+++ client/src/static/scripts/ProjectsInfo.js
@@ -13,7 +13,7 @@ const portfolioProjects = {
     site: 'https://github.com/Diaresta/DRAFTBOYS',
     image: '/images/projects/draftboys-ex.jpg',
     about:
-      "ge.teller is a tool to assist players in traversing the video game OldSchool RuneScape's Grand Exchange. Data is congregated from the official OldSchool RuneScape and OSRS Wiki APIs to provide players with up to date item information. While other Grand Exchange websites offer users item data updates within a week, they lock everything sooner behind a paywall. ge.teller prides itself in giving users free up to date item data within the minute. Using React for the front end, real-time updates are provided via a Node.js/Express server. ge.teller's buy/sell feature provides players a medium to make mock item transactions.  These purchases are stored on a MongoDB database, allowing users to track their purchase history.",
+      "Draftboys is a Magic: The Gathering draft simulator, allowing a player to partake in draft gameplay as if they're seated around a table with friends. Draftboys uses Handlebars as a view engine as well as pure JavaScript for the front end, with a Node.js/Express server and MongoDB database. Foregoing external APIs, Draftboys’ client-side stores all MTG card sets as JSON Objects, allowing quick and dynamic retrieval of cards to be drafted. When a user completes a draft, Draftboys allows them to download their cards as a text file. These text files can then be easily imported into companion software such as MTG Arena and MTG Online, allowing users to play full Magic: The Gathering games online with their decks created on Draftboys.",
     code: 'https://github.com/Diaresta/DRAFTBOYS',
     tech: [
       'ES6 JavaScript',
